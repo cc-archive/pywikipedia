daafb37b9ec4b78ae0e7ed71af880f4afbcd5ded
nyergler <nyergler@committer.creativecommons.org>
2008-04-22 17:14:04 +0000

    Teamspace support.

---
+++ families/teamspace_family.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8  -*-
+
+import family
+
+# The official Mozilla Wiki. #Put a short project description here.
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'teamspace' #Set the family name; this should be the same as in the filename.
+        self.langs = {
+            'en': 'teamspace.creativecommons.org', #Put the hostname here.
+            }
+        self.namespaces[4] = {
+            '_default': u'Teamspace', #Specify the project namespace here. Other
+            }                               #namespaces will be set to MediaWiki default.
+        
+        self.namespaces[5] = {
+            '_default': u'Teamspace talk',
+            }
+        
+    def version(self, code):
+        return "1.11.2"  #The MediaWiki version used. Not very important in most cases.
+    
+    def path(self, code):
+        return '/index.php' #The path of index.php
+
+    def apipath(self, code):
+        return '/api.php'
