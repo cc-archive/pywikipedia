81e3f4932b1733b6642c88cbab589670ef4b6d8e
paulproteus <paulproteus@committer.creativecommons.org>
2008-08-25 21:16:37 +0000

    First attempt at slurping the page into email

--- weekly_teamspace_page_creator.py
+++ weekly_teamspace_page_creator.py
@@ -3,22 +3,28 @@ import wikipedia
 import datetime
 import urllib
 
-def create_page(site, weekday_that_today_should_be=1):
+PAGE_NAME_FORMAT='Staff_updates_%Y-%m-%d'
+
+def create_page(site, weekday_that_today_should_be=1, dry_run = False):
     # This runs in the morning.
     today = datetime.date.today()
     assert (today.weekday() == weekday_that_today_should_be) # 1 is Tuesday
     tomorrow = today + datetime.timedelta(days=1)
 
-    page_name = unicode(tomorrow.strftime('Staff_updates_%Y-%m-%d'))
+    page_name = unicode(tomorrow.strftime(PAGE_NAME_FORMAT))
     page_contents = unicode(today.strftime('Updates for the week ending %Y-%m-%d'))
     
     assert import_from_dir.get_page_contents(site, page_name) is None
     page = wikipedia.Page(site, page_name)
 
-    # Note: This is deliciously racey, since MW has no sense of locking or atomic transactions.  Oh well.
-    page.put(newtext=page_contents,
-             comment = 'Created a staff updates page.',
-             minorEdit = False)
+    if dry_run:
+        print 'Would have put this:'
+        print page_contents
+    else:
+        # Note: This is deliciously racey, since MW has no sense of locking or atomic transactions.  Oh well.
+        page.put(newtext=page_contents,
+                 comment = 'Created a staff updates page.',
+                 minorEdit = False)
 
     return page_name
 
@@ -42,7 +48,28 @@ CC Staff Call Bot.
 
     return msg % (normal_url, add_yours)
 
-def main():
+def get_this_weeks_staff_call_page(site):
+    # This runs in the morning.
+    today = datetime.date.today()
+    # Spin until we see a Wednesday (day==2)
+    wednesday = today
+    while wednesday.weekday() != 2:
+        wednesday += datetime.timedelta(days=1)
+
+    # Great, Wednesday.
+    page_name = wednesday.strftime(PAGE_NAME_FORMAT)
+    return import_from_dir.get_page_contents(site, page_name)
+
+def main(argv):
     site = wikipedia.getSite()
-    page_name = create_page(site, 0)
-    print generate_email(page_name)
+    if argv[0] == 'ask_people_to_fill_in_page':
+        page_name = create_page(site, 0, dry_run = True)
+        body = generate_email(page_name)
+        print body
+    elif argv[0] == 'send_weekly_status_updates':
+        body = get_this_weeks_staff_call_page(site)
+        print body
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
